6d71ce2be96c8aa9d02fb10734c7791867d05553
ninthshark <kiattisak.dujda@gmail.com>
2017-02-04 12:00:02 +0000

    added project detail section

--- project_card.js
+++ project_card.js
@@ -24,6 +24,14 @@ var cardController = (function() {
 
 	var allData = [];
 
+	var calculateScore = function() {
+		var sum = 0;
+		projectData.taskDetail.forEach( function(element) {
+			sum += element.score;
+		});
+		projectData.currentScore = sum;
+	}
+
 	return {
 
 		addProject: function(name,targetScore) {
@@ -37,13 +45,61 @@ var cardController = (function() {
 			}
 
 			project = new CreateProject(ID, name, targetScore);
+			project.addProjectTask();
 			allData.push(project);
 
 			return project;
 		},
 
-		test: function() {
+		addTask: function(task, score) {
+			var ID, newProjectTask;
+
+			if (projectData.taskDetail.length > 0) {
+				ID = projectData.taskDetail[projectData.taskDetail.length - 1].task_id + 1;
+			} else {
+				ID = 0;
+			}
+
+			newProjectTask = new ProjectTask(ID, task, score);
+			projectData.taskDetail.push(newProjectTask);
+			calculateScore();
+			return newProjectTask;
+
+		},
+
+		getLastProjectID: function() {
+			return allData[allData.length - 1].project_id;
+		},
+
+
+		addTaskToProject: function(projectID,task) {
+			var projectIndex = allData.map(function(element) {
+				return element.project_id;
+			}).indexOf(projectID);
+
+			allData[projectIndex].projectTask.push(task);			
+		},
+
+		getProject: function(projectID) {
+			
+		},
+
+		getProjectScore: function() {
+			return {
+				currentScore: projectData.currentScore
+			};
+		},
+
+		// addTaskToProject: function(projectID, task) {
+		// 	allData[0].projectTask.push(task);
+		// },
+
+		testProject: function() {
 			console.log(allData);
+		},
+
+		testTask: function() {
+			console.log(projectData);
 		}
 	}
 
@@ -63,29 +119,99 @@ var UIController = (function() {
 		btnSubmit: '.submit-project',
 		popupNewProject: '#popup-card',
 		projectList: '#project-list',
-		formSubmit: '#form-submit'
+		formSubmit: '#form-submit',
+		projectContent: '.project-content',
+		projectTitle: '.project-title',
+		targetScore: '.target-score-num',
+		projectID: '#project-id',
+
+		//project task
+		taskEditor: '.task_editor',
+		submitButton: '.myButton',
+    	textAreaBox: '.task_text',
+	    taskScore: '.score_box',
+	    displayDescription: '.task_description',
+	    displayScore: '.task_score',
+	    displayTaskList: '.task_list',
+	    currentScore: '.current_score_num'
 	};
 
 
 
 	return {
 
+		// Get project name and target score from input
 		getProjectInput: function() {
 			return {
 				name: document.querySelector(DOMstrings.projectName).value,
 				targetScore: document.querySelector(DOMstrings.projectScore).value
 			};
 		},
-
-		addProjectList: function(obj) {
+		// Add Project to the list
+		addProjectToProjectList: function(obj) {
 
 			var element, html, newHtml;
 			element = DOMstrings.projectList;
-			html = '<li class="box" id="card%id%"><a class="card-open-project" href="#">%project-name%</a></li>';
+			html = '<li class="box" ><a class="card-open-project" id="card-%id%" href="#">%project-name%</a></li>';
 			newHtml = html.replace('%id%', obj.project_id);
 			newHtml = newHtml.replace('%project-name%',obj.name);
 			document.querySelector(element).insertAdjacentHTML('beforeend',newHtml);
 		},
+
+		addProjectTaskSection: function(obj) {
+			var name, target, projectID;
+			name = DOMstrings.projectTitle;
+			target = DOMstrings.targetScore;
+			projectID = DOMstrings.projectID;
+			document.querySelector(name).textContent = obj.name;
+			document.querySelector(target).textContent = obj.target;
+			document.querySelector(projectID).textContent = obj.project_id;
+		},
+
+		//********************* Project task detail******************************************
+		//***********************************************************************************
+		getInput: function() {
+
+	      return {
+	        task: document.querySelector(DOMstrings.textAreaBox).value,
+	        score: parseFloat(document.querySelector(DOMstrings.taskScore).value)
+      		};
+     	},
+
+     	addTaskItem: function(obj) {
+     		var html, newHtml, element;
+     		element = DOMstrings.displayTaskList
+     		html = '<div class="task_item"><div class="task_description">%task description%</div><div class="task_score">%score%</div></div>'
+     		newHtml = html.replace('%task description%',obj.task);
+     		newHtml = newHtml.replace('%score%',obj.score);
+
+     		document.querySelector(element).insertAdjacentHTML('beforeend',newHtml);
+     	},
+
+     	displayCurrentScore: function(obj) {
+     		document.querySelector(DOMstrings.currentScore).textContent = obj.currentScore;
+     	},
+
+     	clearField: function() {
+		    var fields, fieldArr;
+		    fields = document.querySelectorAll('.task_text' + ',' + '.score_box');
+		    fieldArr = Array.prototype.slice.call(fields);
+		    fieldArr.forEach(function(element,index){
+		    	element.value = "";
+		    });
+		      fieldArr[0].focus();
+		},
+
+		clearProjectTask: function() {
+			var fields, fieldsArr;
+			fields = document.querySelectorAll(DOMstrings.displayDescription + ',' + DOMstrings.displayScore);
+			fieldsArr = Array.prototype.slice.call(fields);
+
+			fieldsArr.forEach(function(element, index) {
+				element.textContent = "";
+			});
+		},
+
 
 		getDOMstrings: function() {
 			return DOMstrings;
@@ -94,23 +220,52 @@ var UIController = (function() {
 
 })();
 
-var appController = (function(cardCtrl, UICtrl) {
 
+//**************************************************************************************************
+//**************************** Application Controller **********************************************
+//**************************************************************************************************
+
+var appController = (function(cardCtrl, UICtrl) {
+	var DOM = UICtrl.getDOMstrings();
 	var setupEventListener = function() {
 
-		var DOM = UICtrl.getDOMstrings();
+		
 
 		document.querySelector(DOM.projectList).addEventListener('click', function(event){
-			var name = event.target.className;
-			if (name === "card-create-new-project") {
+			var name = event.target.id;
+			console.log(name);
+			if (name === "card") {
 
 				document.querySelector(DOM.popupNewProject).style.display = "block";
 
 				// change type submit to just button to prevent page reload.
 				document.querySelector(DOM.btnSubmit).addEventListener('click', addNewProject);
+				
+			}
+			else {
+				var projectID = name.split('-')[1]
+				console.log(projectID);
+				
 			}
 
+			// } else if (name === "card-open-project") {
+			// 	document.querySelector(DOM.projectContent).style.display = "block";
+			// }
+
 		});
+		// Listening on project task button
+		// Listening to mouse click event on myButton
+	    document.querySelector(DOM.submitButton).addEventListener('click', controlAddTask);
+
+	    //Listening to enter key event
+	    document.querySelector(DOM.taskEditor).addEventListener('keypress',function(event) {
+	      if (event.keycode === 13 || event.which === 13) {
+	          controlAddTask();
+	      }
+	    });
+
+		
+
 		cardToggle();
 
 	};
@@ -141,9 +296,53 @@ var appController = (function(cardCtrl, UICtrl) {
 	var addNewProject = function() {
 		var projectInput = UICtrl.getProjectInput();
 		var newProject = cardCtrl.addProject(projectInput.name, projectInput.targetScore);
-		UICtrl.addProjectList(newProject);
+		UICtrl.addProjectToProjectList(newProject);
+		
+		document.querySelector(DOM.projectContent).style.display = "block";
+		UICtrl.addProjectTaskSection(newProject);
+		UICtrl.clearProjectTask();
+		
 
-	}; 
+	};
+
+	//find the last project id
+	var thisProjectID = function() {
+		var li_element = document.getElementsByTagName('li');
+		return (li_element[li_element.length - 1].id);
+	}
+
+	// Get project ID
+
+	var getProjectID = function() {
+		var ID = document.getElementById('project-id').textContent;
+		return parseFloat(ID);
+	}
+
+	//**************** Project task ********************************
+	var updateScore = function() {
+
+	    var score = cardCtrl.getProjectScore()
+	    console.log(score);
+	    UICtrl.displayCurrentScore(score);
+  	};
+
+  	var controlAddTask = function() {
+	    var input, newTask, projectID;
+	    input = UICtrl.getInput();
+	    console.log(input);
+	    projectID = getProjectID();
+
+	    if (input.taskl !== "" && !isNaN(input.score)) {
+	      	newTask = cardCtrl.addTask(input.task,input.score);
+	      	UICtrl.addTaskItem(newTask);
+	      	updateScore();
+	      	UICtrl.clearField();
+
+	      	cardCtrl.addTaskToProject(projectID, newTask);
+	     // }
+	 	}
+
+  	};
 
 	return {
 		init: function() {
@@ -156,42 +355,50 @@ var appController = (function(cardCtrl, UICtrl) {
 appController.init();
 
 
-var addProject = function(name,targetScore) {
-	var ID, project;
-	if (allData.length > 0) {
-		ID = allData[allData.length - 1].project_id + 1;
-	} else {
-		ID = 0;
-	}
 
-	project = new CreateProject(ID, name, targetScore);
-	allData.push(project);
-	return project;
-}; 
 
-var addTask = function(task, score) {
-	var ID, newProjectTask;
 
-	if (projectData.taskDetail.length > 0) {
-		ID = projectData.taskDetail[projectData.taskDetail.length - 1].task_id + 1;
-	} else {
-		ID = 0;
-	}
 
-	newProjectTask = new ProjectTask(ID, task, score);
-	projectData.taskDetail.push(newProjectTask);
-	return newProjectTask;
 
-}
 
-var addTaskToProject = function(projectID,task) {
-	var projectIndex = allData.map(function(element) {
-		return element.project_id;
-	}).indexOf(projectID);
 
-	allData[projectIndex].addProjectTask();
-	// allData[projectIndex].projectTask.push(task);
-};
+
+// var addProject = function(name,targetScore) {
+// 	var ID, project;
+// 	if (allData.length > 0) {
+// 		ID = allData[allData.length - 1].project_id + 1;
+// 	} else {
+// 		ID = 0;
+// 	}
+
+// 	project = new CreateProject(ID, name, targetScore);
+// 	allData.push(project);
+// 	return project;
+// }; 
+
+// var addTask = function(task, score) {
+// 	var ID, newProjectTask;
+
+// 	if (projectData.taskDetail.length > 0) {
+// 		ID = projectData.taskDetail[projectData.taskDetail.length - 1].task_id + 1;
+// 	} else {
+// 		ID = 0;
+// 	}
+
+// 	newProjectTask = new ProjectTask(ID, task, score);
+// 	projectData.taskDetail.push(newProjectTask);
+// 	return newProjectTask;
+
+// }
+
+// var addTaskToProject = function(projectID,task) {
+// 	var projectIndex = allData.map(function(element) {
+// 		return element.project_id;
+// 	}).indexOf(projectID);
+
+// 	allData[projectIndex].addProjectTask();
+// 	// allData[projectIndex].projectTask.push(task);
+// };
 
 
 // var newProject = addProject("Portfolio Website", 100);
